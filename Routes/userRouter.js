72a8b09137ff3df6c754b6e9073926336e355811
tharidhu1225 <jayasooriyatharidhu@gmail.com>
2025-01-28 08:38:19 +0530

    delete user

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser, loginUser } from "../controlers/userController.js";
+import { createUser, deleteUser, loginUser } from "../controlers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
+userRouter.delete("/", deleteUser);
 
 export default userRouter;
