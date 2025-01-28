ae9305636ea750eb8d8892f4dcdd88947d22e6c6
tharidhu1225 <jayasooriyatharidhu@gmail.com>
2025-01-28 11:07:09 +0530

    env config

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ import mongoose from "mongoose";
 import userRouter from "./Routes/userRouter.js";
 import productRouter from "./Routes/ptoductRouter.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config()
     
 
 
 const app = express();
 
-const mongoUrl = "mongodb+srv://admin:123@cluster0.h1yaw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoUrl = process.env.MONGO_DB_URI
 
 mongoose.connect(mongoUrl,{})
 
