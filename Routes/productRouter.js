7a629b0c66fb14cee79c4e7fe9317d2ae997da1d
tharidhu1225 <jayasooriyatharidhu@gmail.com>
2025-02-03 18:58:57 +0530

    update productRouter

--- Routes/productRouter.js
+++ Routes/productRouter.js
@@ -5,7 +5,7 @@ const productRouter = express.Router();
 
 productRouter.post("/",createProduct);
 productRouter.get("/",getProducts);
-productRouter.get("/:productId",getProductById)
+productRouter.get("/productId",getProductById)
 productRouter.delete("/:productId",deleteProduct);
 productRouter.put("/:productId",updateProduct);
 
