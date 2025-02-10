eb9747d4f774c066a116f4fc4c5d958bc7835339
tharidhu1225 <jayasooriyatharidhu@gmail.com>
2025-02-10 17:59:48 +0530

    update order Router

--- Routes/orderRouter.js
+++ Routes/orderRouter.js
@@ -6,6 +6,6 @@ const orderRouter = express.Router();
 
 orderRouter.post("/", createOrder)
 orderRouter.get("/", getOrders)
-orderRouter.get("/quote", getQuote)
+orderRouter.post("/quote", getQuote)
 
 export default orderRouter;
